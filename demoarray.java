d6c3bbcd326891d7c515f23d070777cb6284433b
JorgeRodCas <jorgerodcas@hotmail.com>
2024-12-13 20:47:16 -0500

    It's my 2nd commit

---
+++ demoarray.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class demoarray {
+    public static void main(String[] args) {
+        int data[] = { 12, 43, 7, 19, 2, 71, 32 };
+        System.out.println(Arrays.toString(data));
+    }
+}
